38d074e21c1052023e4d584623ea3a7dbfb33b1d
Colin Surprenant <colin.surprenant@gmail.com>
2014-10-19 17:04:19 -0400

    new Mmap::Bytebuffer method signatures, added size & each methods for Enumerable support, some DRYing

--- lib/paged_queue.rb
+++ lib/paged_queue.rb
@@ -2,195 +2,252 @@
 
 require "jruby-mmap"
 
-class PageCache
+module Mmap
+  class PageCache
 
-  def initialize(cache_size, page_base_fname, page_size)
-    @cache_size = cache_size
-    @page_base_fname = page_base_fname
-    @page_size = page_size
-    @cache = {}
-    @num_pages = 0
-  end
-
-  def page(index)
-    @cache[index] || add(index)
-  end
-
-  def close
-    @cache.values.each{|page| page.close}
-  end
-
-  private
-
-  def add(index)
-    # TBD uncache, LRU?
-    @num_pages += 1
-    page = Mmap.new("#{@page_base_fname}.#{index}", @page_size)
-    page.load
-    @cache[index] = page
-  end
-end
-
-class MappedMetadata
-
-  # meta structure
-  # 00: head page index  (long 8 bytes)
-  # 08: head page offset (int  4 bytes)
-  # 12: tail page index  (long 8 bytes)
-  # 20: tail page offset (int  4 bytes)
-
-  def initialize(fname)
-    @meta = Mmap.new(fname, 2048) # you know, the constant
-    @meta.load
-  end
-
-  def head_page_index
-    @head_page_index ||= @meta.get_long_at(0)
-  end
-
-  def head_page_index=(index)
-    @meta.put_long_at(0, index)
-    @head_page_index = index
-  end
-
-  def head_page_offset
-    @head_page_offset ||= @meta.get_int_at(8)
-  end
-
-  def head_page_offset=(offset)
-    @meta.put_int_at(8, offset)
-    @head_page_offset = offset
-  end
-
-  def tail_page_index
-    @tail_page_index ||= @meta.get_long_at(12)
-  end
-
-  def tail_page_index=(index)
-    @meta.put_long_at(12, index)
-    @tail_page_index = index
-  end
-
-  def tail_page_offset
-    @tail_page_offset ||= @meta.get_int_at(20)
-  end
-
-  def tail_page_offset=(offset)
-    @meta.put_int_at(20, offset)
-    @tail_page_offset = offset
-  end
-
-  def close
-    @meta.close
-  end
-end
-
-# PagedQueue
-# non blocking, non thread-safe persistent queue implementation using mmap
-class PagedQueue
-  INT_BYTES = 4
-  LONG_BYTES = 8
-
-  # data structure
-  # 0: data size (int  4 bytes)
-  # 4: data (data size in bytes)
-  # ...
-  #
-  # a data size of 0 signal the end of the page
-
-  def initialize(fname, page_size)
-    @fname = fname
-    @page_size = page_size
-    @page_max_data_size = @page_size - (2 * INT_BYTES)
-
-    @meta = MappedMetadata.new(@fname)
-    @cache = PageCache.new(2, @fname, @page_size)
-
-    # prime the cache with tail & head pages
-    @cache.page(@meta.head_page_index)
-    @cache.page(@meta.tail_page_index)
-  end
-
-  # @param data [String] write the data string backing bytes to queue
-  # @return [Integer] number of data bytes written
-  def push(data)
-    size = data.bytesize
-    raise("data size=#{data.bytesize} is larger than configured page usable size=#{@page_max_data_size} (#{@page_size} - #{2 * INT_BYTES})") if size > @page_max_data_size
-
-    offset = @meta.head_page_offset
-    if size > (@page_max_data_size - offset)
-      # data size is larger than usable data size left, move head to next page
-      @meta.head_page_index += 1
-      @meta.head_page_offset = offset = 0
+    def initialize(cache_size, page_base_fname, page_size)
+      @cache_size = cache_size
+      @page_base_fname = page_base_fname
+      @page_size = page_size
+      @cache = {}
+      @num_pages = 0
     end
-    page = @cache.page(@meta.head_page_index)
-    page.position = offset
-    page.put_int(size)
-    page.put_bytes(data)
 
-    # write a trailing 0 after the data. on next push, this will be overwritten with next data size
-    # since offset will be set on this trailing zero, otherwise if we changed page, trailing zero
-    # will be set.
-    page.put_int(0)
-    @meta.head_page_offset = offset + size + INT_BYTES
+    def page(index)
+      @cache[index] || add(index)
+    end
 
-    size
+    def close
+      @cache.values.each{|page| page.close}
+    end
+
+    private
+
+    def add(index)
+      # TBD uncache, LRU?
+      @num_pages += 1
+      page = Mmap::ByteBuffer.new("#{@page_base_fname}.#{index}", @page_size)
+      page.load
+      @cache[index] = page
+    end
   end
-  alias_method :<<, :push
 
-  # skip is like pop but does not read the data, just skip over it
-  # @return [Integer] number of data bytes skipped
-  def skip
-    offset = @meta.tail_page_offset
-    index = @meta.tail_page_index
-    return nil if index >= @meta.head_page_index && offset >= @meta.head_page_offset
-    page = @cache.page(index)
+  class MappedMetadata
 
-    page.position = offset
+    # meta structure
+    # 00: head page index  (long 8 bytes)
+    # 08: head page offset (int  4 bytes)
+    # 12: tail page index  (long 8 bytes)
+    # 20: tail page offset (int  4 bytes)
+    # 24: size             (int  4 byte)
 
-    if (size = page.get_int) == 0
-      # we hit the trailing zero that indicates the end of data on this page
-      # TBD refactor, duplicate code
-      offset = @meta.tail_page_offset = 0
-      index = @meta.tail_page_index += 1
+    def initialize(fname)
+      existing = File.exists?(fname)
+      @meta = Mmap::ByteBuffer.new(fname, 2048) # you know, the constant
+      @meta.load
+      clear unless existing
+    end
+
+    def head_page_index
+      @head_page_index ||= @meta.get_long(0)
+    end
+
+    def head_page_index=(index)
+      @meta.put_long(index, 0)
+      @head_page_index = index
+    end
+
+    def head_page_offset
+      @head_page_offset ||= @meta.get_int(8)
+    end
+
+    def head_page_offset=(offset)
+      @meta.put_int(offset, 8)
+      @head_page_offset = offset
+    end
+
+    def tail_page_index
+      @tail_page_index ||= @meta.get_long(12)
+    end
+
+    def tail_page_index=(index)
+      @meta.put_long(index, 12)
+      @tail_page_index = index
+    end
+
+    def tail_page_offset
+      @tail_page_offset ||= @meta.get_int(20)
+    end
+
+    def tail_page_offset=(offset)
+      @meta.put_int(offset, 20)
+      @tail_page_offset = offset
+    end
+
+    def size
+      @size ||= @meta.get_int(24)
+    end
+    alias_method :length, :size
+
+    def size=(n)
+      @meta.put_int(n, 24)
+      @size = n
+    end
+    alias_method :length=, :size=
+
+    def clear
+      self.head_page_index = 0
+      self.head_page_offset = 0
+      self.tail_page_index = 0
+      self.tail_page_offset = 0
+      self.size = 0
+    end
+
+    def close
+      @meta.close
+    end
+  end
+
+  class PagedQueueError < StandardError; end
+
+  # PagedQueue
+  # non blocking, non thread-safe persistent queue implementation using mmap
+  class PagedQueue
+    include Enumerable
+
+    attr_reader :meta, :page_usable_size
+
+    INT_BYTES = 4
+    LONG_BYTES = 8
+
+    # data structure
+    # 0: data size (int  4 bytes)
+    # 4: data (data size in bytes)
+    # ...
+    #
+    # a data size of 0 signal the end of the page
+
+    # TBD: add caching/paging class parameter which will allow choosing/defining
+    # page management strategies, for example, in a sized queue we may want to reuse
+    # pages as in a circular buffer since we know the queue size is limited
+
+    def initialize(fname, page_size)
+      @fname = fname
+      @page_size = page_size
+      @page_usable_size = @page_size - (2 * INT_BYTES)
+
+      @meta = MappedMetadata.new(@fname)
+      @cache = PageCache.new(2, @fname, @page_size)
+
+      # prime the cache with tail & head pages
+      @cache.page(@meta.head_page_index)
+      @cache.page(@meta.tail_page_index)
+    end
+
+    # @param data [String] write the data string backing bytes to queue
+    # @return [Integer] number of data bytes written
+    def push(data)
+      size = data.bytesize
+      raise(PagedQueueError, "data size=#{data.bytesize} is larger than usable page size=#{@page_usable_size} (#{@page_size} - #{2 * INT_BYTES})") if size > @page_usable_size
+
+      offset = @meta.head_page_offset
+      if size > (@page_usable_size - offset)
+        # data size is larger than usable data size left, move head to next page
+        @meta.head_page_index += 1
+        @meta.head_page_offset = offset = 0
+      end
+      page = @cache.page(@meta.head_page_index)
+      page.position = offset
+      page.put_int(size)
+      page.put_bytes(data)
+
+      # write a trailing 0 after the data. on next push, this will be overwritten with next data size
+      # since offset will be set on this trailing zero, otherwise if we changed page, trailing zero
+      # will be set.
+      page.put_int(0)
+      @meta.head_page_offset = offset + size + INT_BYTES
+      @meta.size += 1
+
+      size
+    end
+    alias_method :<<, :push
+
+    # skip is like pop but does not read the data, just skip over it
+    # @return [Integer] number of data bytes skipped
+    def skip
+      forward(false)
+    end
+
+    # @return [String] retrieve data bytes from queue
+    def pop
+      forward(true)
+    end
+    alias_method :shift, :pop
+
+    # iterate & yield over each queue item without pop'ing them
+    def each
+      offset = @meta.tail_page_offset
+      index = @meta.tail_page_index
+
+      while true
+        return if index >= @meta.head_page_index && offset >= @meta.head_page_offset
+
+        page = @cache.page(index)
+        page.position = offset
+
+        if (size = page.get_int) == 0
+          # we hit the trailing zero that indicates the end of data on this page
+          offset = 0
+          index += 1
+          return if index >= @meta.head_page_index && offset >= @meta.head_page_offset
+
+          page = @cache.page(index)
+          page.position = offset
+          size = page.get_int
+        end
+
+        offset = offset + size + INT_BYTES
+
+        yield page.get_bytes(size)
+      end
+    end
+
+    def size
+      @meta.size
+    end
+
+    def close
+      @meta.close
+      @cache.close
+    end
+
+    private
+
+    # @param read [Boolean] physically read & return the data or not
+    # @return [String|Integer|nil] nil if no items, data String if read == true otherwise size of data
+    def forward(read = true)
+      offset = @meta.tail_page_offset
+      index = @meta.tail_page_index
       return nil if index >= @meta.head_page_index && offset >= @meta.head_page_offset
+
       page = @cache.page(index)
       page.position = offset
-      size = page.get_int
+
+      if (size = page.get_int) == 0
+        # we hit the trailing zero that indicates the end of data on this page
+        offset = @meta.tail_page_offset = 0
+        index = @meta.tail_page_index += 1
+        return nil if index >= @meta.head_page_index && offset >= @meta.head_page_offset
+
+        page = @cache.page(index)
+        page.position = offset
+        size = page.get_int
+      end
+
+      @meta.tail_page_offset = offset + size + INT_BYTES
+      @meta.size -= 1
+
+      read ? page.get_bytes(size) : size
     end
-
-    @meta.tail_page_offset = offset + size + INT_BYTES
-
-    size
-  end
-
-  # @return [String] retrieve data bytes from queue
-  def pop
-    offset = @meta.tail_page_offset
-    index = @meta.tail_page_index
-    return nil if index >= @meta.head_page_index && offset >= @meta.head_page_offset
-    page = @cache.page(index)
-
-    page.position = offset
-
-    if (size = page.get_int) == 0
-      # we hit the trailing zero that indicates the end of data on this page
-      # TBD refactor, duplicate code with skip
-      offset = @meta.tail_page_offset = 0
-      index = @meta.tail_page_index += 1
-      return nil if index >= @meta.head_page_index && offset >= @meta.head_page_offset
-      page = @cache.page(index)
-      page.position = offset
-      size = page.get_int
-    end
-
-    @meta.tail_page_offset = offset + size + INT_BYTES
-    page.get_bytes(size)
-  end
-  alias_method :shift, :pop
-
-  def close
-    @meta.close
-    @cache.close
   end
 end
